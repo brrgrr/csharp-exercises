7a0be15cfdcdf6d2fd2cf443df2e97a3af89eb0d
Eric Berger <eric@brrgrr.com>
2017-11-06 06:36:46 -0600

    Class 1

---
+++ Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;
+                        
+            Console.WriteLine("Enter a radius:");
+            string input = Console.ReadLine();
+            float radius = float.Parse(input);
+            var area = pi * radius * radius;
+            Console.WriteLine(string.Format("The area of a circle with radius {0} is: {1}",radius, area));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LiveCodeFunTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LiveCodeFunTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new double();
+            result = 1;
+            Console.WriteLine("Press any key...");
+            var key = Console.ReadKey();
+            Console.WriteLine(key);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
